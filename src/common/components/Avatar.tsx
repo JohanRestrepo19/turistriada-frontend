89dadcb69fbafc47eda92bf60dbd592a379d7b2b
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2023-05-14 22:43:15 -0500

    feat(users): finish UserNavbar component.

--- src/common/components/Avatar.tsx
+++ src/common/components/Avatar.tsx
@@ -1,23 +1,37 @@
-interface AvartarProps {
+import classNames from 'classnames'
+import { BaseHTMLAttributes } from 'react'
+
+interface AvartarProps extends BaseHTMLAttributes<HTMLDivElement> {
   imgSrc?: string
   height?: number
   width?: number
 }
 
-export const Avatar = ({ imgSrc, height = 75, width = 75 }: AvartarProps) => {
+export const Avatar = ({
+  className,
+  imgSrc,
+  height = 75,
+  width = 75
+}: AvartarProps) => {
+  const avatarStyles = classNames({
+    avatar: true,
+    [`${className}`]: className,
+    placeholder: !imgSrc
+  })
+
   if (imgSrc)
     return (
-      <div className="avatar">
+      <div className={avatarStyles}>
         <div className="w-12 rounded-full">
-          <img src={imgSrc} />
+          <img src={imgSrc} height={height} width={width} />
         </div>
       </div>
     )
 
   return (
-    <div className="avatar placeholder">
+    <div className={avatarStyles}>
       <div className="bg-neutral-focus text-neutral-content rounded-full w-12">
-        <span>MX</span>
+        <span>DA</span>
       </div>
     </div>
   )
--- src/common/components/UserNavbar.tsx
+++ src/common/components/UserNavbar.tsx
@@ -4,16 +4,16 @@ import { Avatar, TuristriadaHeading } from './'
 export const UserNavbar = () => {
   return (
     <>
-      <div className="w-full bg-navbar/80 ">
-        <div className="container mx-auto flex flex-wrap justify-between items-center text-white font-semibold py-2">
+      <div className="w-full bg-navbar/80 px-2">
+        <div className="flex flex-col lg:flex-row justify-between items-center text-white font-semibold py-2">
           <TuristriadaHeading width={76} />
-          <nav className="grow flex flex-wrap gap-x-12 justify-end items-center">
+          <nav className="grow flex flex-row flex-wrap justify-evenly md:justify-end items-center gap-x-4">
             <NavLink to="">Recomendaciones</NavLink>
             <NavLink to="">Promociones</NavLink>
             <NavLink to="">¿Sabías qué?</NavLink>
             <NavLink to="">Categorias</NavLink>
             <button>Logout</button>
-            <Avatar imgSrc="https://www.google.com/imgres?imgurl=https%3A%2F%2Fe0.pxfuel.com%2Fwallpapers%2F889%2F641%2Fdesktop-wallpaper-aesthetic-tiktok-aesthetic-profile-pic.jpg&tbnid=o1_M1q3e1QEWVM&vet=12ahUKEwjKrITI_PX-AhXXnoQIHffyBR8QMygOegUIARCCAg..i&imgrefurl=https%3A%2F%2Fwww.pxfuel.com%2Fen%2Fquery%3Fq%3Dprofile%2Baesthetic&docid=5oitQR5VLit6tM&w=850&h=1509&q=profile%20pictures&ved=2ahUKEwjKrITI_PX-AhXXnoQIHffyBR8QMygOegUIARCCAg" />
+            <Avatar className="hidden lg:block" />
           </nav>
         </div>
       </div>
