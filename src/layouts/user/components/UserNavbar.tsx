88c07730181890e39763a63eff9158a7d45ce8a0
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2023-05-18 21:52:20 -0500

    style(users): change active url style.

--- src/layouts/user/components/UserNavbar.tsx
+++ src/layouts/user/components/UserNavbar.tsx
@@ -2,16 +2,27 @@ import { NavLink } from 'react-router-dom'
 import { Avatar, TuristriadaHeading } from '@/common/components'
 
 export const UserNavbar = () => {
+  const handleActiveClass = ({ isActive }: { isActive: boolean }) =>
+    isActive ? 'text-primary' : ''
+
   return (
     <>
       <div className="w-full bg-navbar/80 px-2">
         <div className="flex flex-col lg:flex-row justify-between items-center text-white font-semibold py-2">
           <TuristriadaHeading width={76} />
           <nav className="grow flex flex-row flex-wrap justify-evenly md:justify-end items-center gap-x-4">
-            <NavLink to="">Recomendaciones</NavLink>
-            <NavLink to="">Promociones</NavLink>
-            <NavLink to="">¿Sabías qué?</NavLink>
-            <NavLink to="">Categorias</NavLink>
+            <NavLink to="/" className={handleActiveClass}>
+              Inicio
+            </NavLink>
+            <NavLink to="/promos" className={handleActiveClass}>
+              Promociones
+            </NavLink>
+            <NavLink to="/curiosities" className={handleActiveClass}>
+              ¿Sabías qué?
+            </NavLink>
+            <NavLink to="/categories" className={handleActiveClass}>
+              Categorias
+            </NavLink>
             <button>Logout</button>
             <Avatar className="hidden lg:block" />
           </nav>
