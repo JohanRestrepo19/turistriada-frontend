8b01e16fc1daf2b8f9eda258c84694c7e7c555ad
Brandddd <b.palacio1@utp.edu.co>
2023-05-23 19:39:40 -0500

    chore(users): Working on users view profile - UserPostsList component added.

--- src/pages/users/UserProfile.tsx
+++ src/pages/users/UserProfile.tsx
@@ -1,8 +1,30 @@
-import { ViewUserProfile } from "./components/ViewUserProfile"
-
+import { User } from "@/common/types"
+import { PersonalInformationCard } from "./components/PersonalInformationCard"
+import { LastestPostsCard } from "./components/LastestPostsCard"
 
 export const UserProfile = () => {
-    return (
-        <ViewUserProfile />
-    )
+    // TODO: Props que van a llegar mas adelante a este componente
+  const user: User = {
+    documentNumber: '1088351988',
+    username: 'Brandddd',
+    name: 'Brandon',
+    lastName: 'Alvarez',
+    email: 'b.palacio1@utp.edu.co',
+    role: 'user',
+    _id: 'ajfdsjk3278ksad',
+    profileImgUrl: 'https://avatars.githubusercontent.com/u/54783819?v=4'
+  }
+
+  return (
+    <>
+      <div className="flex">
+        <div className="mx-4">
+          <PersonalInformationCard user={user} />
+        </div>
+        <div className="mx-4">
+          <LastestPostsCard />
+        </div>
+      </div>
+    </>
+  )
 }
---
+++ src/pages/users/components/UserPostsGrid.tsx
@@ -0,0 +1,3 @@
+export const UserPostsGrid = () => {
+  return <div className="text-center">User Posts Grid Component</div>
+}
