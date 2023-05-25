abc8cf4c0f7d893bfc7f7348d6699e2cc36b6044
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2023-05-25 17:55:13 -0500

    chore(places): wip over PlaceDetails.

--- src/pages/places/PlaceDetails.tsx
+++ src/pages/places/PlaceDetails.tsx
@@ -3,9 +3,11 @@ import { PlaceImageSlider } from './components/PlaceImageSlider'
 import { selectPlace } from '@/store/slices/placesSlice'
 import { PlaceDescription } from './components/PlaceDescription'
 import { MakePlaceReview } from './components/MakePlaceReview'
+import { Place } from '@/common/types'
 
 export const PlaceDetails = () => {
-  const place = useAppSelector(selectPlace)
+  //TODO: Verify if there is a selectPlace if not then request to firebase
+  const place = useAppSelector(selectPlace) as Place
 
   return (
     <div className="grid sm:grid-cols-1 lg:grid-cols-2 gap-y-8 gap-x-4 py-8 w-10/12">
@@ -14,7 +16,7 @@ export const PlaceDetails = () => {
       {/* 2. Images slider */}
       <PlaceImageSlider imgSrc={place?.imgUrl || ''} />
       {/* 3. Description panel */}
-      <PlaceDescription />
+      <PlaceDescription place={place} />
       {/* 4. Make a Review */}
       <MakePlaceReview />
 
--- src/pages/places/components/PlaceDescription.tsx
+++ src/pages/places/components/PlaceDescription.tsx
@@ -1,12 +1,58 @@
-export const PlaceDescription = () => {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faComments, faContactBook } from '@fortawesome/free-regular-svg-icons'
+import { faGlobe } from '@fortawesome/free-solid-svg-icons'
+import { IconProp } from '@fortawesome/fontawesome-svg-core'
+import { Place } from '@/common/types'
+
+interface PlaceFieldProps {
+  name: string
+  content: string
+  icon: IconProp
+}
+const PlaceField = ({ content, name, icon }: PlaceFieldProps) => {
+  return (
+    <div className="grid grid-cols-12">
+      {/* Icon follow by field description */}
+
+      <div className="col-span-4">
+        <span className="text-primary">
+          <FontAwesomeIcon icon={icon} size="xl" />
+        </span>
+        <h3 className="text-primary-text font-semibold inline ml-4">{name}</h3>
+      </div>
+      <span className="col-span-8 text-center border-b border-secondary-text">
+        {content}
+      </span>
+    </div>
+  )
+}
+
+interface PlaceDescriptionProps {
+  place: Place
+}
+
+export const PlaceDescription = ({ place }: PlaceDescriptionProps) => {
   return (
     <div className="card bg-white shadow-xl h-[300px]">
-      <div className="card-body">
-        <h2 className="card-title">Card title!</h2>
-        <p>If a dog chews shoes whose shoes does he choose?</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
-        </div>
+      <div className="card-body gap-y-4 overflow-y-scroll">
+        {/*Place field*/}
+        <PlaceField name="Nombre" content={place.name} icon={faComments} />
+        <PlaceField
+          name="Descripción"
+          content={place.description}
+          icon={faContactBook}
+        />
+        <PlaceField
+          name="Descripción"
+          content={place.description}
+          icon={faContactBook}
+        />
+        <PlaceField
+          name="Descripción"
+          content={place.description}
+          icon={faContactBook}
+        />
+        <PlaceField name="Dirección" content={place.location} icon={faGlobe} />
       </div>
     </div>
   )
