ceaf830b3423f5441a4cc7f0c584b21c6f1d6892
Brandddd <b.palacio1@utp.edu.co>
2023-05-25 11:38:22 -0500

    feat(users): grid user view for user profile implemented

--- src/pages/users/components/UserPostsGrid.tsx
+++ src/pages/users/components/UserPostsGrid.tsx
@@ -6,13 +6,13 @@ interface UserPostsGridProps {
 
 export const UserPostsGrid = ({ placeList }: UserPostsGridProps) => {
   return (
-    <div className="flex flex-row items-start justify-center mt-4">
-      <div className="card rounded-lg w-36 bg-white text-secondary-text shadow-xl border-2 border-primary-light hover:scale-105 hover:border-accent ease-in-out duration-300">
-        <h2 className="font-normal text-center mx-auto justify-center py-2">
-          {placeList.name}
-        </h2>
-        <img src={placeList.imgUrl} className="w-full h-auto" alt="place" />
+    <div className="card card-compact w-48 bg-primary-content shadow-xl mx-2">
+      <div className="card-body text-center">
+        <h2 className="text-base font-semibold">{placeList.name}</h2>
       </div>
+      <figure>
+        <img className="" src={placeList.imgUrl} alt="Places" />
+      </figure>
     </div>
   )
 }
