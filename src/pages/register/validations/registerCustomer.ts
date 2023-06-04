6759d4ef27a02392d165815b483b46c8c14029e4
Karen Grajales <karen.grajales@utp.edu.co>
2023-06-04 07:54:44 -0500

    refactor: add new document types and put an asterik in required fields.

--- src/pages/register/validations/registerCustomer.ts
+++ src/pages/register/validations/registerCustomer.ts
@@ -3,24 +3,24 @@ import { yupResolver } from '@hookform/resolvers/yup'
 
 const registerCustomerSchema = object({
   nit: string()
-    .min(8, 'El NIT debe tener minimo 8 digitos')
+    .min(8, 'El NIT debe tener mínimo 8 dígitos.')
     .required('El campo "NIT" es obligatorio.'),
   commercialRegistration: string().required(
-    'El campo "Registro mercantil" es obligatorio.'
+    'Por favor ingresa el registro mercantil de tu empresa.'
   ),
-  companyName: string().required('Por favor ingresa el nombre de tu compañía.'),
-  location: string().required('Por favor ingresa la direccion de tu compañía.'),
+  companyName: string().required('Por favor ingresa el nombre de tu empresa.'),
+  location: string().required('Por favor ingresa la direccion de tu empresa.'),
   phone: string()
-    .min(7, 'Tu telefono debe ser de al menos 7 digitos.')
+    .min(7, 'Tu teléfono debe tener al menos 7 dígitos.')
     .required('Por favor ingresa tu numero de celular.'),
-  email: string().email().required('Por favor ingresa tu correo'),
-  username: string().required('Por favor ingresa tu username.'),
+  email: string().email().required('Por favor ingresa tu correo electrónico.'),
+  username: string().required('Por favor ingresa tu nombre de usuario.'),
   password: string()
-    .min(8, 'Tu contraseña debe tener al menos 8 digitos')
-    .required('La contraseña es requerida'),
+    .min(8, 'Tu contraseña debe tener al menos 8 dígitos.')
+    .required('La contraseña es requerida.'),
   confirmPassword: string().oneOf(
     [ref('password')],
-    'Las constraseñas no coinciden'
+    'Las constraseñas no coinciden.'
   )
 })
 
