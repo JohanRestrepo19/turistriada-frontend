cb1e840d905a2fd6fcc3a805a47fee4d68decea5
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2023-06-03 23:48:24 -0500

    refactor(promos): change url path to promos page.

--- src/pages/promos/ViewPromos.tsx
+++ src/pages/promos/ViewPromos.tsx
@@ -1,11 +1,10 @@
-import { PromosList } from "./components/PromosList"
+import { PromosList } from './components/PromosList'
 
-
-export const ViewPromo = () => {
+export const ViewPromos = () => {
   return (
     <div>
       <h3 className="text-primary my-9 text-4xl font-medium leading-tight text-center">
-          Promociones
+        Promociones
       </h3>
       <PromosList />
     </div>
