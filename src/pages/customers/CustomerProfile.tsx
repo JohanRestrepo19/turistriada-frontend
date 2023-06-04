c56704e3153f9ff1b95bd48905fdeb2ce188bb31
Johan Restrepo <62318029+JohanRestrepo19@users.noreply.github.com>
2023-06-04 16:02:26 -0500

    refactor(customers): remove useless fetch logic.

--- src/pages/customers/CustomerProfile.tsx
+++ src/pages/customers/CustomerProfile.tsx
@@ -1,34 +1,16 @@
-import { useEffect, useState } from 'react'
-import { toast } from 'react-toastify'
 import { CustomerPromotions } from './components/CustomerPromotions'
-import { Loader } from '@/common/components'
 import { PersonalInformationCard } from './components/PersonalInformationCard'
-import { getCustomerById } from '@/services/firebase'
 import { selectAuthUser } from '@/store/slices/authSlice'
 import { useAppSelector } from '@/common/hooks'
 import type { Customer } from '@/common/types'
 
 export const CustomerProfile = () => {
   const authUser = useAppSelector(selectAuthUser) as Customer
-  const [isLoading, setIsLoading] = useState<boolean>(true)
-  const [customer, setCustomer] = useState<Customer | null>(null)
-
-  useEffect(() => {
-    const fetchCustomerInfo = async () => {
-      const response = await getCustomerById(authUser._id)
-      if (response.hasError) return toast.error(response.errorMsg)
-      setCustomer(response.customer as Customer)
-      setIsLoading(false)
-    }
-    fetchCustomerInfo()
-  }, [authUser._id])
-
-  if (isLoading) return <Loader />
   return (
     <>
       <div className="flex">
         <div className="mx-4">
-          <PersonalInformationCard customer={customer as Customer} />
+          <PersonalInformationCard customer={authUser as Customer} />
         </div>
         <div className="mx-4 w-auto">
           <CustomerPromotions />
